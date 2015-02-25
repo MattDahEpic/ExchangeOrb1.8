3271a034939a7e5341fee8301b8276fda9e073fa
Matt <mattdahepic@gmail.com>
2015-02-24 17:09:21 -0700

    Initial commit, updated 1.7.10 to 1.8

---
+++ src/main/java/com/mattdahepic/exchangeorb/ExchangeOrb.java
@@ -0,0 +1,52 @@
+package com.mattdahepic.exchangeorb;
+
+import com.mattdahepic.exchangeorb.config.Config;
+import com.mattdahepic.exchangeorb.item.ItemExchangeOrb;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraft.item.Item;
+import net.minecraftforge.common.config.Configuration;
+import org.apache.logging.log4j.LogManager;
+
+@Mod(modid = ExchangeOrb.MODID, name = ExchangeOrb.NAME, version = ExchangeOrb.VERSION,guiFactory = "com.mattdahepic.exchangeorb.config.ExchangeOrbConfigFactory")
+public class ExchangeOrb {
+    @Mod.Instance("exchangeorb")
+    public static ExchangeOrb instance;
+
+    public static final String MODID = "exchangeorb";
+    public static final String VERSION = "1.8-1.6";
+    public static final String NAME = "Exchange Orb";
+
+    @SidedProxy(clientSide = "com.mattdahepic.exchangeorb.client.ClientProxy", serverSide = "com.mattdahepic.exchangeorb.CommonProxy")
+    public static CommonProxy proxy;
+    //public static ClientProxy clientProxy;
+
+    public static Configuration configFile;
+
+    //blocks
+
+    //item
+    public static Item itemExchangeOrb;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        FMLCommonHandler.instance().bus().register(instance);
+        Config.load(event);
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        itemExchangeOrb = new ItemExchangeOrb();
+        proxy.registerBlocksItems();
+        proxy.registerRecipes();
+        proxy.registerRenderers();
+    }
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        LogManager.getLogger().info("Ready to transmute!");
+    }
+}
---
+++ src/main/java/com/mattdahepic/exchangeorb/item/ItemExchangeOrb.java
@@ -0,0 +1,40 @@
+package com.mattdahepic.exchangeorb.item;
+
+import com.mattdahepic.exchangeorb.config.Config;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class ItemExchangeOrb extends Item {
+    public ItemExchangeOrb () {
+        super();
+        this.setUnlocalizedName("exchangeOrb");
+        this.setMaxStackSize(1);
+        this.setCreativeTab(CreativeTabs.tabAllSearch);
+        if(Config.orbHasDurability && Config.orbDurability > 1 ) { //if durability enabled and not one use
+            this.setMaxDamage(Config.orbDurability-1);
+            System.out.println("orb has a durability of " + this.getMaxDamage(new ItemStack(this)));
+        }
+        //TODO: creative tabs
+    }
+    /*@Override
+    public boolean doesContainerItemLeaveCraftingGrid(ItemStack itemStack) {
+        return false;
+    }*/
+    @Override
+    public ItemStack getContainerItem(ItemStack stack) {
+        if(isDamageable()) { //if more than one use, but not infinite
+            //System.out.println("input stack damage is: " + stack.getItemDamage() + " and output damage is: " + stack.getItemDamage()+1);
+            //System.out.println("returning damaged stack");
+            return new ItemStack(stack.getItem(),1,stack.getItemDamage()+1);
+        } else if (Config.orbDurability == 1) { //if one use
+            //System.out.println("one use, removing item");
+            return null;
+        } else { //if infinite
+            //System.out.println("infinite, returning input");
+            return stack;
+        }
+    }
+}
